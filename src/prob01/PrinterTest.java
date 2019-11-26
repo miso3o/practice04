22569c8c295fee388b5b06b72fa001f4e671a32a
BIT <BIT@DESKTOP-DOGVD1T>
2019-11-26 19:05:33 +0900

    오류부분 일시적 주석화(나중에 문제풀때 풀기)

--- src/prob01/PrinterTest.java
+++ src/prob01/PrinterTest.java
@@ -5,10 +5,10 @@ public class PrinterTest {
 	public static void main(String[] args) {
 		Printer printer = new Printer();
 
-		printer.println( 10 );
-		printer.println( true );
-		printer.println( 5.7 );
-		printer.println( "홍길동" );
+//		printer.println( 10 );
+//		printer.println( true );
+//		printer.println( 5.7 );
+//		printer.println( "홍길동" );
 	}
 
 }
--- src/prob05/Prob05.java
+++ src/prob05/Prob05.java
@@ -3,10 +3,10 @@ package prob05;
 public class Prob05 {
 
 	public static void main(String[] args) {
-		Base base = new MyBase();
-
-		base.service("낮");
-		base.service("밤");
-		base.service("오후"); 
+//		Base base = new MyBase();
+//
+//		base.service("낮");
+//		base.service("밤");
+//		base.service("오후"); 
 	}
 }
